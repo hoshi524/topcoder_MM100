9d16620f702503f2b1d45c595418427282e2c99f
hoshi524 <hoshi524@gmail.com>
2018-04-20 05:32:14 +0900

    update

--- run.py
+++ run.py
@@ -8,20 +8,9 @@ def solve(seed):
     return float(subprocess.check_output('java -cp out SameColorPairsVis -exec out/main.out -seed {}'.format(seed), shell=True))
 
 testcase = 100
-filename = 'max.json'
 rate = 0
-
-file = open(filename, 'r')
-max = json.loads(file.read())
-file.close()
 
 for seed in range(1, testcase):
     score = solve(seed)
-    if max[seed] < score:
-        max[seed] = score
     rate = (rate * (seed - 1) + score) / seed
     print('{}\t{:.6f}\t{:.6f}'.format(seed, score, rate))
-
-file = open(filename, 'w')
-file.write(json.dumps(max))
-file.close()
